bc53717f9d930e4591cbf9fa7050c1740c991ddb
Jacob <jacobwroberts33@gmail.com>
2023-03-09 16:22:35 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+const messageNames = [];
+
+function writeCards(names, value) {
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+    console.log(countDown--)
+}
+}
